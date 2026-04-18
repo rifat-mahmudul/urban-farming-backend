2fd47f7658f84dfa1788257a28d11b7ed658da58
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-04-18 09:10:22 +0600

    auth middleware done

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status-codes";
+import { verifyToken } from "../utils/jwt";
+import AppError from "../../errors/appError";
+import { jwtConfig } from "../config/jwtConfig";
+
+export const auth =
+  (...requiredRoles: string[]) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization;
+
+      if (!token) {
+        throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized");
+      }
+
+      const verifiedToken = verifyToken(token, jwtConfig.accessToken) as any;
+
+      req.user = verifiedToken;
+
+      if (requiredRoles.length && !requiredRoles.includes(verifiedToken.role)) {
+        throw new AppError(httpStatus.FORBIDDEN, "Forbidden access");
+      }
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
