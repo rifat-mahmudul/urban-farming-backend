f453281c268c8f8b8acca28e3bc891d9d4f9ff61
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-04-18 14:03:18 +0600

    authLimiter added

--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { AuthControllers } from "./auth.controller";
+import { authLimiter } from "../../middlewares/rateLimiter";
 
 const router = Router();
 
-router.post("/register", AuthControllers.register);
-router.post("/login", AuthControllers.login);
+router.post("/register", authLimiter, AuthControllers.register);
+router.post("/login", authLimiter, AuthControllers.login);
 
 export const AuthRoutes = router;
